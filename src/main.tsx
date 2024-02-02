98521544bde56723c3f683c127eac76cc1cae184
yuto ichinose <yuto.0526.sano@gmail.com>
2024-02-02 09:48:21 +0900

    fix: `ColorModeScript`追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { UIProvider } from '@yamada-ui/react';
+import { ColorModeScript, UIProvider } from '@yamada-ui/react';
 import { customConfig } from './theme/config.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={customConfig.initialColorMode} />
     <UIProvider config={customConfig}>
       <App />
     </UIProvider>
