130e6da7146b08bf721e7837b3e5878c40f7350a
illionillion <yuto.0526.sano@gmail.com>
2025-07-06 14:18:57 +0900

    refactor: 変数名変更

--- src/App.tsx
+++ src/App.tsx
@@ -14,23 +14,23 @@ import type { FC } from 'react';
 import { useEffect, useState } from 'react';
 import { Cell } from './components/Cell';
 
-const defaultArr: number[][] = new Array(3).fill(new Array(3).fill(0));
+const defaultCells: number[][] = new Array(3).fill(new Array(3).fill(0));
 /**
  * 0: 継続, 1: 勝ち, 2: 負け, 3: 引き分け
  */
 type resultType = 0 | 1 | 2 | 3;
 const App: FC = () => {
   const { toggleColorMode } = useColorMode();
-  const [flg1] = useMediaQuery('(min-width: 1000px)');
-  const [flg2] = useMediaQuery('(min-width: 500px)');
-  const [arr, setArr] = useState<number[][]>(defaultArr);
+  const [isLargeScreen] = useMediaQuery('(min-width: 1000px)');
+  const [isMediumScreen] = useMediaQuery('(min-width: 500px)');
+  const [cells, setCells] = useState<number[][]>(defaultCells);
   const [isPlayer, setIsPlayer] = useState<boolean>(false);
   const [result, setResult] = useState<resultType>(0);
   const handleClick = (i: number, j: number) => {
     if (result !== 0) return;
-    setArr((newArr) =>
-      newArr.map((r, ni) =>
-        ni === i ? r.map((c, nj) => (nj === j ? (!isPlayer ? 1 : -1) : c)) : r,
+    setCells((newCells) =>
+      newCells.map((cell, index) =>
+        index === i ? cell.map((c, nj) => (nj === j ? (!isPlayer ? 1 : -1) : c)) : cell,
       ),
     );
     setIsPlayer(!isPlayer);
@@ -38,25 +38,25 @@ const App: FC = () => {
 
   const winLoseCheck = () => {
     // 横判定
-    if (arr.map((r) => r.every((c) => c === 1)).some((v) => v === true)) {
+    if (cells.map((r) => r.every((c) => c === 1)).some((v) => v === true)) {
       console.log('1の勝ち');
       setResult(1);
       return;
     }
-    if (arr.map((r) => r.every((c) => c === -1)).some((v) => v === true)) {
+    if (cells.map((r) => r.every((c) => c === -1)).some((v) => v === true)) {
       console.log('-1の勝ち');
       setResult(2);
       return;
     }
 
     // 縦判定
-    for (let i = 0; i < arr.length; i++) {
-      if (arr.map((r) => r[i]).every((v) => v === 1)) {
+    for (let i = 0; i < cells.length; i++) {
+      if (cells.map((r) => r[i]).every((v) => v === 1)) {
         console.log('1の勝ち');
         setResult(1);
         return;
       }
-      if (arr.map((r) => r[i]).every((v) => v === -1)) {
+      if (cells.map((r) => r[i]).every((v) => v === -1)) {
         console.log('-1の勝ち');
         setResult(2);
         return;
@@ -64,31 +64,31 @@ const App: FC = () => {
     }
 
     // 斜めの判定
-    if (arr.map((r, i) => r[i]).every((v) => v === 1)) {
+    if (cells.map((r, i) => r[i]).every((v) => v === 1)) {
       console.log('1の勝ち');
       setResult(1);
       return;
     }
-    if (arr.map((r, i) => r[i]).every((v) => v === -1)) {
+    if (cells.map((r, i) => r[i]).every((v) => v === -1)) {
       console.log('-1の勝ち');
       setResult(2);
       return;
     }
 
     // 斜めの判定（反対）
-    if (arr.map((r, i) => r[r.length - 1 - i]).every((v) => v === 1)) {
+    if (cells.map((r, i) => r[r.length - 1 - i]).every((v) => v === 1)) {
       console.log('1の勝ち');
       setResult(1);
       return;
     }
-    if (arr.map((r, i) => r[r.length - 1 - i]).every((v) => v === -1)) {
+    if (cells.map((r, i) => r[r.length - 1 - i]).every((v) => v === -1)) {
       console.log('-1の勝ち');
       setResult(2);
       return;
     }
 
     // 引き分けの判定
-    if (arr.map((e) => e.every((v) => v !== 0)).every((v) => v === true)) {
+    if (cells.map((e) => e.every((v) => v !== 0)).every((v) => v === true)) {
       console.log('引き分け');
       setResult(3);
       return;
@@ -101,10 +101,10 @@ const App: FC = () => {
   const reStart = () => {
     setIsPlayer(false);
     setResult(0);
-    setArr(defaultArr);
+    setCells(defaultCells);
   };
 
-  useEffect(winLoseCheck, [arr]);
+  useEffect(winLoseCheck, [cells]);
 
   return (
     <Center w='100vw' h='100dvh' flexDir='column' gap={3}>
@@ -112,11 +112,11 @@ const App: FC = () => {
       <NativeTable
         withBorder
         withColumnBorders
-        w={`calc(100vw * ${flg1 ? 0.25 : flg2 ? 0.5 : 0.75})`}
-        h={`calc(100vw * ${flg1 ? 0.25 : flg2 ? 0.5 : 0.75})`}
+        w={`calc(100vw * ${isLargeScreen ? 0.25 : isMediumScreen ? 0.5 : 0.75})`}
+        h={`calc(100vw * ${isLargeScreen ? 0.25 : isMediumScreen ? 0.5 : 0.75})`}
       >
         <Tbody>
-          {arr.map((r, i) => (
+          {cells.map((r, i) => (
             <Tr key={i}>
               {r.map((c, j) => (
                 <Cell {...{ c, i, j, handleClick, key: j }} />
@@ -139,7 +139,7 @@ const App: FC = () => {
           }
         })()}
       </Text>
-      <VStack w={`calc(100vw * ${flg1 ? 0.25 : flg2 ? 0.5 : 0.75})`}>
+      <VStack w={`calc(100vw * ${isLargeScreen ? 0.25 : isMediumScreen ? 0.5 : 0.75})`}>
         <Button onClick={toggleColorMode}>テーマ切り替え</Button>
         {result !== 0 && <Button onClick={reStart}>もう一回</Button>}
       </VStack>
